ca0e987a97846d963cc7657720ed4b67f24419b2
eepuribharath <eepuribharath@gmail.com>
2026-06-25 11:42:43 +0530

    19.05.2026

---
+++ tasks/19.05.2026/positive number or not.py
@@ -0,0 +1,6 @@
+n=int(input("Enter a value :-"))
+
+if n>=0:
+    print(n,"is Positive Number")
+else:
+    print(n,"is not a positive Number")
---
+++ tasks/19.05.2026/upper case or not.py
@@ -0,0 +1,7 @@
+char=input("Enter a character :")
+if char >= "A" and char<="Z":
+    print("Upper Case")
+elif char>="a" and char<="z":
+    print("Lower Case")
+else:
+    print("Not a alphabet")
